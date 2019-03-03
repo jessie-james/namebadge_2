0f9bcac5e69b2a1741cad7b31246367c459a49e4
Jessica James <jessicajames@Jessicas-MacBook-Air.local>
2019-03-03 08:18:25 -0700

    change 1

--- problems.js
+++ problems.js
@@ -1,11 +1,11 @@
 ////////data problem 9 ////////////////////////////////////////
-"Instructions"
-"Make a function called favFood that does not take in any parameters.
-"The function should return a string that has a value of  "My favorite food is pizza"
-Instead of explicitly typing "pizza", make reference to the existing variable called food.
+//Instructions"
+//Make a function called favFood that does not take in any parameters.
+//The function should return a string that has a value of  "My favorite food is pizza"
+//Instead of explicitly typing "pizza", make reference to the existing variable called food.
 
-Use template strings.
-Do not use concatenation ("My favorite food is " + food)""
+//Use template strings.
+//Do not use concatenation ("My favorite food is " + food)""
 ///////////////////////////////////////////////////////////////////
 
 var food = "pizza";
@@ -17,9 +17,9 @@ function favFood() {
 //////////////////////////////////////////////////////
 
 ////////////// Function Practice Problem 6/////////////////
-Instructions:
-Create a function called makeExercise that has two parameters-- exerciseName(string) and duration(number).
-The function should return an object with two key-value pairs. The first property name should be "exerciseName" with a value of the first parameter. The second property name should be "duration" with a value of the second parameter.
+//Instructions:
+//Create a function called makeExercise that has two parameters-- exerciseName(string) and duration(number).
+//The function should return an object with two key-value pairs. The first property name should be "exerciseName" with a value of the first parameter. The second property name should be "duration" with a value of the second parameter.
 //////////////////////////////////////////////
 
 var makeExercise = {
@@ -33,13 +33,10 @@ var makeExercise = {
 //////////////////////////////////////////////////////
 
 ///////////////If Statement Practice Problem 9///////////////
-
-Instructions Instructions
-
-Write a function called makePurchase that takes in a parameter called priceOfItem. If the priceOfItem is less than or equal to the total on the user object, have the makePurchase function subtract the priceOfItem from the current users total. If the priceOfItem is greater than the total in the user object then have the function return "not enough funds".
-
-Use the if and else keywords in your final solution.
-The arguments passed into the makePurchase function will always be numbers.
+//Instructions Instructions
+//Write a function called makePurchase that takes in a parameter called priceOfItem. If the priceOfItem is less than or equal to the total on the user object, have the makePurchase function subtract the priceOfItem from the current users total. If the priceOfItem is greater than the total in the user object then have the function return "not enough funds".
+//Use the if and else keywords in your final solution.
+//The arguments passed into the makePurchase function will always be numbers.
 
 
 //////////////////////////////////////////////////////
